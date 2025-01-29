a6b4e4e908cd3343665cab7dc71c7accea31c405
Raquel Gonçalves <rfg@ua.pt>
2025-01-29 20:09:13 +0000

    Update load_dataset

--- load_dataset.py
+++ load_dataset.py
@@ -1,22 +1,69 @@
 import argparse
 import os
 from pymgrid import Microgrid
-from pymgrid.modules import BatteryModule, LoadModule, RenewableModule
+from pymgrid.modules import BatteryModule, LoadModule, RenewableModule, GridModule
 import yaml
+import pandas as pd
 
-parser = argparse.ArgumentParser(description='Load a dataset')
+parser = argparse.ArgumentParser(description='Load a dataset', formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('-d', '--dataset', type=str, help='Path to dataset root', required=True)
-parser.add_argument('-b', '--battery', type=str, help='Path to battery yaml file', required=True)
-parser.add_argument('-m', '--microgrid', type=str, help='Path to save the microgrid yaml file', required=True)
+parser.add_argument('-b', '--battery', type=str, help='Path to battery settings (.yaml)', required=True)
+parser.add_argument('-c', '--config', type=str, help='Path to create the environment', required=True)
+parser.add_argument('-m', '--microgrid', type=str, help='Path to save the microgrid (.yaml)', required=True)
 
+msg = '''
+The dataset should have the following structure:
+dataset/
+├── GridModule/
+│   └── grid.csv
+├── LoadModule/           # Multiple load files are allowed
+│   ├── load[...].csv
+│   ├── load[...].csv
+│   └── ...
+└── RenewableModule/      # Multiple renewable files are allowed
+    ├── pv[...].csv
+    ├── wind[...].csv
+    └── ...
+'''
+
+parser.epilog = msg
 args = parser.parse_args()
 
-for module in ['GridModule', 'LoadModule', 'RenewableModule']:
-    if os.path.exists(os.path.join(args.dataset, module)):
-        pass
+grid = None
+loads = []
+renewables = []
 
+config = yaml.safe_load(open(args.config, 'r'))
+
+for module in ['grid', 'loads', 'renewables']:
+    if os.path.exists(os.path.join(args.dataset, module)):
+        if module == 'grid':
+            filename = os.listdir(os.path.join(args.dataset, module))[0]
+            ts = pd.read_csv(os.path.join(args.dataset, module, filename), index_col=0)
+
+            grid = GridModule(
+                max_export=config['max_export'],
+                max_import=config['max_import'],
+                time_series=ts,
+            )
+
+        elif module in {'loads', 'renewables'}:
+            for filename in os.listdir(os.path.join(args.dataset, module)):
+                ts = pd.read_csv(os.path.join(args.dataset, module, filename), index_col=0)
+
+                if module == 'loads':
+                    loads.append(
+                        LoadModule(time_series=ts)
+                    )
+
+                elif module == 'renewables':
+                    src_type = filename.split('_')[0]
+
+                    renewables.append(
+                        (src_type, RenewableModule(time_series=ts))
+                    )
     else:
-        print(f'{module} not found. Exiting...')
+        print(f'{module} directory not found. Exiting...')
         exit(1)
 
 
@@ -25,4 +72,9 @@ batteries = [
     for b in yaml.safe_load(open(args.battery, 'r')).values()
 ]
 
-# microgrid = Microgrid([*batteries, *renewables, *loads, grid])
+microgrid = Microgrid([*batteries, *renewables, *loads, grid])
+
+os.makedirs(args.microgrid, exist_ok=True)
+microgrid.dump(open(os.path.join(args.microgrid, 'env.yaml'), 'w'))
+
+print(f'Microgrid saved to {args.microgrid}')
